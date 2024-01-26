f8866d16b74be4fbfcc6c0e4320ef0d83bc5bc32
Manthan Patel <manthan.patel@sick.de>
2024-01-26 16:06:33 +0100

    edit val normalize

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -43,9 +43,9 @@ class DetectionValidator(BaseValidator):
 
     def preprocess(self, batch):
         """Preprocesses batch of images for YOLO training."""
-        batch['img'][:,0,:,:] = batch['img'][:,0,:,:] / 31500 # intensity image 255
-        batch['img'][:,1,:,:]  = batch['img'][:,1,:,:] / 255 # depth image 31500
-        batch['img'] = batch['img'].to(self.device, non_blocking=True)
+        # batch['img'][:,0,:,:] = batch['img'][:,0,:,:] / 31500 # intensity image 255
+        # batch['img'][:,1,:,:]  = batch['img'][:,1,:,:] / 255 # depth image 31500
+        # batch['img'] = batch['img'].to(self.device, non_blocking=True)
         batch['img'] = (batch['img'].half() if self.args.half else batch['img'].float())
         for k in ['batch_idx', 'cls', 'bboxes']:
             batch[k] = batch[k].to(self.device)
